82b78071924e575c217e8e572073c550cdc733e7
August Seiple <aseiple678@gmail.com>
2016-08-03 09:25:32 -0400

    Refactoring

--- FeatureDetection.m
+++ FeatureDetection.m
@@ -1,14 +1,7 @@
     clear; clc; close all;
 
     trainImage = rgb2gray(imread('TrainingMats/catinhat.jpg'));
-    % figure;
-    % imshow(trainImage);
-    % title('Training Image');
-
     testImage = rgb2gray(imread('testmat.jpg'));
-    % figure;
-    % imshow(testImage);
-    % title('Testing Image');
 
     trainPoints = detectSURFFeatures(trainImage);
     testPoints = detectSURFFeatures(testImage);
@@ -21,34 +14,25 @@
 
     figure;
     imshow(testImage);
-    title('1000 Strongest Features');
+    title('3000 Strongest Features');
     hold on;
-    plot(selectStrongest(testPoints, 1000));
+    plot(selectStrongest(testPoints, 3000));
     % export_fig print.png -native
 
     [trainFeatures, trainPoints] = extractFeatures(trainImage, trainPoints);
     [testFeatures, testPoints] = extractFeatures(testImage, testPoints);
 
     imgPairs = matchFeatures(trainFeatures, testFeatures);
-
     matchedTrainingPoints = trainPoints(imgPairs(:, 1), :);
     matchedTestingPoints = testPoints(imgPairs(:, 2), :);
-    % figure;
-    % showMatchedFeatures(trainImage, testImage, matchedTrainingPoints, matchedTestingPoints, 'montage');
-    % title('Putatively Matched Points (Including Outliers)');
 
     [tform, inlierTrainPoints, inlierTestPoints] = estimateGeometricTransform(matchedTrainingPoints, matchedTestingPoints, 'affine');
 
     figure;
     showMatchedFeatures(trainImage, testImage, inlierTrainPoints, inlierTestPoints, 'montage');
-    title('Matched Points');
+    title('Matching Points');
 
-    boxPolygon = [1, 1; ...
-        size(trainImage, 2), 1; ...
-        size(trainImage, 2), size(trainImage, 1); ...
-        1, size(trainImage, 1); ...
-        1, 1];
-
+    boxPolygon = [1, 1; size(trainImage, 2), 1; size(trainImage, 2), size(trainImage, 1); 1, size(trainImage, 1); 1, 1];
     newBoxPolygon = transformPointsForward(tform, boxPolygon);
 
     figure;
